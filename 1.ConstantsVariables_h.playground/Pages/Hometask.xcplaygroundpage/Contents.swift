6453a79b03dcfb487f12fc71ffaa656ebdda4783
Roman Brovko <brovko-roman@ya.ru>
2018-03-28 21:14:39 +0300

    lesson 1 home work

---
+++ 1.ConstantsVariables_h.playground/Pages/Hometask.xcplaygroundpage/Contents.swift
@@ -0,0 +1,51 @@
+// максимально упростите код
+
+
+// 1
+
+// let i:Double = 5.0;
+
+let i = 5.0
+
+
+// 2
+
+/*
+ var c1:Int;
+ var c2:Int;
+ var c3:Int;
+ var c4:Int;
+ */
+
+var c1, c2, c3, c4:Int
+
+
+// 3
+
+/*
+
+let str:String
+str = "150"
+
+*/
+
+let str = "150"
+
+// 4
+
+
+/*
+
+ var str2:String = "text"
+ var str3:String = "text"
+ var str4:String = "text"
+
+ */
+
+let str2="text", str3="text", str4 = "text"
+
+//let str3 = str2
+//let str4 = str2
+
+
+
